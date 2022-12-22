74ad52f71e47c77c26c8567f9318a355c03c977e
mishoto <mihail.dev.io@gmail.com>
2022-12-22 17:55:49 +0200

    add implementation for withProperties & with Datasource methods

--- src/main/java/lib/ORManager.java
+++ src/main/java/lib/ORManager.java
@@ -1,4 +1,38 @@
 package lib;
 
+import configs.HikariCPDataSource;
+import configs.PropertyConfiguration;
+
+import javax.sql.DataSource;
+import java.nio.file.Path;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public interface ORManager {
+
+    static ORManager withPropertiesFrom(String filename) {
+        Properties properties = PropertyConfiguration.readPropertiesFromFile(Path.of(filename));
+        Connection connection = null;
+        try (Connection conn = DriverManager
+                .getConnection(properties.getProperty("orm.connection.url"),
+                        properties.getProperty("orm.connection.username"),
+                        properties.getProperty("orm.connection.password"));){
+            connection = conn;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ORManagerImpl(connection);
+    }
+
+    static ORManager withDataSource(DataSource dataSource) {
+        Connection connection = null;
+        try {
+            connection = HikariCPDataSource.getHikariDatasourceConfiguration(dataSource).getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return new ORManagerImpl(connection);
+    }
 }
