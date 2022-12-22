9a039c373ffe311065f80163e3e37fa63c75f911
mishoto <mihail.dev.io@gmail.com>
2022-12-22 17:58:32 +0200

    create PropertiesConfiguration class

---
+++ src/main/java/configs/PropertyConfiguration.java
@@ -0,0 +1,35 @@
+package configs;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Properties;
+
+import static java.lang.System.err;
+
+public class PropertyConfiguration {
+
+    private PropertyConfiguration() {}
+
+    public static Properties readPropertiesFromFile(Path file){
+        Properties properties = new Properties();
+        try(InputStream inputStream = getFileFromResourceAsInputStream(file.toString())) {
+            properties.load(inputStream);
+        } catch (IOException ioException) {
+            err.println("Error when reading properties from file");
+            ioException.printStackTrace();
+        }
+        return properties;
+    }
+
+    private static InputStream getFileFromResourceAsInputStream(String fileName){
+
+        ClassLoader classLoader = PropertyConfiguration.class.getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            return inputStream;
+        }
+    }
+}
