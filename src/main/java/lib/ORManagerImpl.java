83bf65a50dde85c316c2be17897f9a58f81a2580
mishoto <mihail.dev.io@gmail.com>
2022-12-22 17:57:00 +0200

    add Connection as field & in the constructor

--- src/main/java/lib/ORManagerImpl.java
+++ src/main/java/lib/ORManagerImpl.java
@@ -1,4 +1,16 @@
 package lib;
 
-public class ORManagerImpl {
+import java.sql.Connection;
+
+public class ORManagerImpl implements ORManager{
+
+    private final Connection connection;
+
+    public ORManagerImpl(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Connection getConnection() {
+        return this.connection;
+    }
 }
