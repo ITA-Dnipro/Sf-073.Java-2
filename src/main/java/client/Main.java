2266c4534c014a00b6e15f7af4e25e0fe567c83b
mishoto <mihail.dev.io@gmail.com>
2022-12-22 17:54:21 +0200

    add execution line for test start the connection

--- src/main/java/client/Main.java
+++ src/main/java/client/Main.java
@@ -1,4 +1,13 @@
 package client;
 
+import com.zaxxer.hikari.HikariDataSource;
+import lib.ORManager;
+import lib.ORManagerImpl;
+
 public class Main {
+
+
+    public static void main(String[] args) {
+        ORManagerImpl orManager = (ORManagerImpl) ORManager.withDataSource(new HikariDataSource());
+    }
 }
