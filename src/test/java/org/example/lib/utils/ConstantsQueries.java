674ef1613c704cff366d918ee2717310e8247a6c
mishoto <mihail.dev.io@gmail.com>
2022-12-28 15:17:02 +0200

    create ConstantsQueries for tests

--- src/test/java/org/example/lib/utils/ConstantsQueries.java
+++ src/test/java/org/example/lib/utils/ConstantsQueries.java
@@ -9,15 +9,24 @@ public class ConstantsQueries {
         private SqlSelectQueries() {
         }
 
-        public static final String SELECT_ALL_BOOKS = "SELECT * FROM book";
-        public static final String SELECT_LAST_BOOK_ID = "SELECT book.id FROM book ORDER BY book.id DESC LIMIT 1";
+        public static final String SELECT_FIRST_BOOK = "SELECT * FROM BOOK LIMIT 1";
+        public static final String SELECT_ALL_FROM_BOOK_BY_ID = "SELECT * FROM BOOK WHERE id=?";
     }
 
     public static final class SqlInsertQueries {
 
         private SqlInsertQueries() {}
 
-        public static final String INSERT_ONE_ENTITY =
-                "INSERT INTO ? (?) VALUES (?)";
+    }
+
+    public static final class SqlUpdateQueries {
+
+        private SqlUpdateQueries() {}
+
+    }
+    public static final class SqlDeleteQueries {
+
+        private SqlDeleteQueries() {}
+
     }
 }
