ab0236a40fb4e968ce66bfee2241d60863fe1ea0
mishoto <mihail.dev.io@gmail.com>
2022-12-28 15:21:53 +0200

    minor refactoring

--- src/test/java/org/example/lib/MethodSaveTest.java
+++ src/test/java/org/example/lib/MethodSaveTest.java
@@ -1,28 +1,24 @@
 package org.example.lib;
 
 import com.zaxxer.hikari.HikariDataSource;
-import org.assertj.db.type.Request;
-import org.assertj.db.type.Source;
+import org.assertj.db.type.*;
+import org.example.configs.PropertyConfiguration;
 import org.example.entity.Book;
-import org.example.lib.utils.Constants;
-import org.example.lib.utils.ConstantsQueries;
-import org.example.lib.utils.EntityUtils;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.example.lib.utils.*;
+import org.junit.jupiter.api.*;
 
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.nio.file.Path;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Properties;
 
 import static org.assertj.db.api.Assertions.assertThat;
 
 class MethodSaveTest {
-
-    private static final String dbURL = "jdbc:h2:file:D:\\0_SoftServe_Academy\\Sf-073.Java-2\\src\\main\\java\\org\\example\\db\\h2-test";
-    private final Source h2Source = new Source(dbURL, "", "");
-    private final Book book = new Book("Hibernate101", LocalDate.of(2000, 10, 05));
+    private static final Path path = Path.of(Constants.Connection.PROPERTIES_FILE_NAME);
+    private static final Properties properties = PropertyConfiguration.readPropertiesFromFile(path);
+    private final Source h2Source = new Source(properties.getProperty(Constants.Connection.URL), "", "");
     private static ORManager orManager;
     private static ORManagerImpl orManagerImpl;
 
@@ -35,31 +31,28 @@ class MethodSaveTest {
         }
     }
 
-    MethodSaveTest() throws SQLException {}
+    MethodSaveTest() throws SQLException {
+    }
 
     @Test
     void should_Check_If_Entity_Exists_In_Db() {
-        Request requestAllBooks = new Request(h2Source, ConstantsQueries.SqlSelectQueries.SELECT_ALL_BOOKS);
-
-        assertThat(requestAllBooks).row(0)
+        Request requestFirstBook = new Request(h2Source, ConstantsQueries.SqlSelectQueries.SELECT_FIRST_BOOK);
+        assertThat(requestFirstBook).row(0)
                 .hasValues(1, "Solaris", "1961-01-01");
     }
 
     @Test
-    void should_Check_If_Method_HasEntityId_Is_Correct() throws Exception {
-
-        Assertions.assertFalse(EntityUtils.hasEntityId(book));
+    void should_Check_If_Method_HasId_Is_Correct() throws Exception {
+        Book book = new Book("Hibernate101", LocalDate.of(2000, 10, 5));
+        Assertions.assertFalse(EntityUtils.hasId(book));
         List<Book> bookList = orManager.findAll(Book.class);
-        Assertions.assertTrue(EntityUtils.hasEntityId(bookList.get(0)));
+        Assertions.assertTrue(EntityUtils.hasId(bookList.get(0)));
 
-        try (PreparedStatement preparedStatement = orManagerImpl.getConnection()
-                .prepareStatement(ConstantsQueries.SqlSelectQueries.SELECT_LAST_BOOK_ID)) {
-            ResultSet resultSet = preparedStatement.executeQuery();
-            long lastRecordId = Long.parseLong(resultSet.getString(1));
-            long newRecordId = lastRecordId + 1;
-        }catch(SQLException ex){
-            ex.printStackTrace();
-        }
+    }
 
+    @Test
+    void should_Retrieve_Column_Names_From_Table() throws Exception {
+        Assertions.assertEquals("TITLE, PUBLISHED_AT", SqlUtils.getColumnNamesInsert("BOOK", orManagerImpl.getConnection()));
+        Assertions.assertEquals("ID, TITLE, PUBLISHED_AT", SqlUtils.getColumnNamesUpdate("BOOK", orManagerImpl.getConnection()));
     }
 }
