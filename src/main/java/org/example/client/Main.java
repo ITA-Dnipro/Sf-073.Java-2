09a8229fbe7302cde0dc373164ae90355289652e
mishoto <mihail.dev.io@gmail.com>
2022-12-30 14:19:40 +0200

    change book test objects

--- src/main/java/org/example/client/Main.java
+++ src/main/java/org/example/client/Main.java
@@ -18,24 +18,23 @@ public class Main {
         orManager.register(Publisher.class);
         orManager.register(Book.class);
 
-        Book book1 = new Book("Solaris", LocalDate.of(1961, 1, 1));
-        Book book2 = new Book("Just Book", LocalDate.now());
+        Book book1 = new Book("Solaris", LocalDate.of(1961, 1, 1), new Publisher("Pub1"));
+        Book book2 = new Book("Just Book", LocalDate.now(), null);
         Book book3 = new Book("Just Book 2", LocalDate.of(1961, 1, 1));
-        Book book4 = new Book("Just Book 3", LocalDate.of(1961, 1, 1));
+
 
         Publisher publisher1 = new Publisher("Just Publisher");
         Publisher publisher2 = new Publisher("MyPub1");
         Publisher publisher3 = new Publisher("MyPub2");
         Publisher publisher4 = new Publisher("MyPub3");
 
-//        orManager.persist(book1);
-//        orManager.persist(book2);
-//        orManager.persist(book3);
-//        orManager.persist(book4);
-//        orManager.persist(publisher1);
-//        orManager.persist(publisher2);
-//        orManager.persist(publisher3);
-//        orManager.persist(publisher4);
+        orManager.persist(book1);
+        orManager.persist(book2);
+        orManager.persist(book3);
+        orManager.persist(publisher1);
+        orManager.persist(publisher2);
+        orManager.persist(publisher3);
+        orManager.persist(publisher4);
 
         System.out.println("Find All Books");
         orManager.findAll(Book.class).forEach(System.out::println);
