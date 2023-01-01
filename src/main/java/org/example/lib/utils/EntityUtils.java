11cdd16405ff02d6e7a19fc89dc6d98af638882e
mishoto <mihail.dev.io@gmail.com>
2023-01-01 10:17:06 +0200

    add helper methods for save

--- src/main/java/org/example/lib/utils/EntityUtils.java
+++ src/main/java/org/example/lib/utils/EntityUtils.java
@@ -4,13 +4,19 @@ import org.example.lib.annotation.*;
 
 import java.lang.reflect.*;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.*;
 import java.util.stream.Collectors;
 
 
 public class EntityUtils {
 
-    private EntityUtils() {}
+    private EntityUtils() {
+    }
 
     public static String getTableName(Class<?> cls) {
         if (cls.isAnnotationPresent(Table.class)) {
@@ -22,7 +28,7 @@ public class EntityUtils {
         return cls.getSimpleName();
     }
 
-    public static  String getFieldName(Field field) {
+    public static String getFieldName(Field field) {
         if (field.isAnnotationPresent(Column.class)) {
             String name = field.getAnnotation(Column.class).name();
             if (!name.equals("")) {
@@ -74,14 +80,14 @@ public class EntityUtils {
                 result.add("'" + value.toString() + "'");
                 result.add(",");
             }
-            if(declaredField.isAnnotationPresent(ManyToOne.class)){
+            if (declaredField.isAnnotationPresent(ManyToOne.class)) {
                 declaredField.setAccessible(true);
                 Object value = declaredField.get(o);
                 result.add(value);
                 result.add(",");
             }
         }
-        result.remove(result.size()-1);
+        result.remove(result.size() - 1);
         return result;
     }
 
@@ -89,7 +95,7 @@ public class EntityUtils {
         boolean isEntity = o.getClass().isAnnotationPresent(Entity.class);
         Long id = null;
         try {
-            if(isEntity) {
+            if (isEntity) {
                 Method getId = o.getClass().getMethod("getId");
                 id = (Long) getId.invoke(o);
             }
@@ -103,7 +109,7 @@ public class EntityUtils {
         boolean isEntity = o.getClass().isAnnotationPresent(Entity.class);
         Long id = null;
         try {
-            if(isEntity) {
+            if (isEntity) {
                 Method getId = o.getClass().getMethod("getId");
                 id = (Long) getId.invoke(o);
             }
@@ -112,4 +118,67 @@ public class EntityUtils {
         }
         return id;
     }
+
+    public static boolean hasIdAndColumnAnnotation(Field field) {
+        return field.isAnnotationPresent(Column.class) ||
+                field.isAnnotationPresent(Id.class);
+    }
+
+    public static boolean hasManyToOneAnnotation(Field field) {
+        return field.isAnnotationPresent(ManyToOne.class);
+    }
+
+    public static <T> PreparedStatement setterPreparedStatementExecution(PreparedStatement statement, ResultSetMetaData resultSetMetaData,T o) throws SQLException, IllegalAccessException {
+        int columnCount = resultSetMetaData.getColumnCount();
+        Field[] fields = o.getClass().getDeclaredFields();
+        Map<String, List<Object>> collectFieldTypeValues = new HashMap<>();
+        for (int i = 1; i <= columnCount; i++) {
+            for (Field field : fields) {
+                field.setAccessible(true);
+                if (hasIdAndColumnAnnotation(field)) {
+                    String[] fieldRawType = field.getType().toString().split(" ");
+                    String fieldTypePretty = fieldRawType[1];
+                    String columnTypeClassName = resultSetMetaData.getColumnClassName(i);
+                    EntityUtils.normalizeSqlToJavaTypesWithValues(collectFieldTypeValues, columnTypeClassName, fieldTypePretty, field, o);
+                }
+                if(hasManyToOneAnnotation(field)){
+                    if(field.get(o) == null){
+                        break;
+                    }
+                    String fieldTypePretty = "entity";
+                    String columnTypeClassName = resultSetMetaData.getColumnClassName(i);
+                    EntityUtils.normalizeSqlToJavaTypesWithValues(collectFieldTypeValues, columnTypeClassName, fieldTypePretty, field, o);
+                }
+            }
+        }
+        for (int i = 1; i+1 <= columnCount; i++) {
+            String columnName = resultSetMetaData.getColumnName(i+1).toLowerCase();
+            if (collectFieldTypeValues.containsKey(columnName)){
+                statement.setObject(i, collectFieldTypeValues.get(columnName).get(1));
+                continue;
+            }
+            statement.setObject(i, null);
+        }
+        return statement;
+    }
+
+    private static Map<String, List<Object>> normalizeSqlToJavaTypesWithValues(Map<String, List<Object>> collectFieldTypeValues, String columnTypeClassName, String fieldRawType, Field field, Object o) throws IllegalAccessException {
+
+        if(columnTypeClassName.equals(fieldRawType) && columnTypeClassName.equals("java.lang.String")){
+            collectFieldTypeValues.put(EntityUtils.getFieldName(field), new ArrayList<>(Arrays.asList("java.lang.String", field.get(o))));
+        }
+        if(columnTypeClassName.equals("java.sql.Date") && fieldRawType.equals("java.time.LocalDate")){
+            collectFieldTypeValues.put(EntityUtils.getFieldName(field), new ArrayList<>(Arrays.asList("java.sql.Date", field.get(o))));
+        }
+        if(columnTypeClassName.equals(fieldRawType) && columnTypeClassName.equals("java.lang.Long")){
+            collectFieldTypeValues.put(EntityUtils.getFieldName(field), new ArrayList<>(Arrays.asList("java.lang.Long", field.get(o))));
+        }
+        if(columnTypeClassName.equals("java.lang.Long") && fieldRawType.equals("entity")){
+            Object associatedObject = field.get(o);
+            Long id = EntityUtils.getId(associatedObject);
+            collectFieldTypeValues.put(EntityUtils.getFieldName(field), new ArrayList<>(Arrays.asList("java.lang.Long", id)));
+        }
+        return collectFieldTypeValues;
+    }
 }
+
